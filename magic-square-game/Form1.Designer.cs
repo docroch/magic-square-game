03663bab08991b3abd6f69dca12cff8e2fe92567
docroch <soheilabbasnia@gmail.com>
2026-06-26 23:03:17 +0330

    Add textboxes, label and buttons with codes for every actions.

--- magic-square-game/Form1.Designer.cs
+++ magic-square-game/Form1.Designer.cs
@@ -28,20 +28,262 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.txt5 = new System.Windows.Forms.TextBox();
+            this.txt6 = new System.Windows.Forms.TextBox();
+            this.txt7 = new System.Windows.Forms.TextBox();
+            this.txt8 = new System.Windows.Forms.TextBox();
+            this.txt9 = new System.Windows.Forms.TextBox();
+            this.btnSolve = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // txt1
+            // 
+            this.txt1.AccessibleName = "txt1";
+            this.txt1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt1.Location = new System.Drawing.Point(29, 24);
+            this.txt1.Margin = new System.Windows.Forms.Padding(5);
+            this.txt1.MaxLength = 1;
+            this.txt1.Multiline = true;
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(126, 82);
+            this.txt1.TabIndex = 0;
+            this.txt1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt1_KeyPress);
+            // 
+            // txt2
+            // 
+            this.txt2.AccessibleName = "txt2";
+            this.txt2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt2.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt2.Location = new System.Drawing.Point(183, 23);
+            this.txt2.Margin = new System.Windows.Forms.Padding(5);
+            this.txt2.MaxLength = 1;
+            this.txt2.Multiline = true;
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(126, 82);
+            this.txt2.TabIndex = 1;
+            this.txt2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt1_KeyPress);
+            // 
+            // txt3
+            // 
+            this.txt3.AccessibleName = "txt3";
+            this.txt3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt3.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt3.Location = new System.Drawing.Point(338, 24);
+            this.txt3.Margin = new System.Windows.Forms.Padding(5);
+            this.txt3.MaxLength = 1;
+            this.txt3.Multiline = true;
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(126, 82);
+            this.txt3.TabIndex = 2;
+            this.txt3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt1_KeyPress);
+            // 
+            // txt4
+            // 
+            this.txt4.AccessibleName = "txt4";
+            this.txt4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt4.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt4.Location = new System.Drawing.Point(29, 123);
+            this.txt4.Margin = new System.Windows.Forms.Padding(5);
+            this.txt4.MaxLength = 1;
+            this.txt4.Multiline = true;
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(126, 82);
+            this.txt4.TabIndex = 3;
+            this.txt4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt1_KeyPress);
+            // 
+            // txt5
+            // 
+            this.txt5.AccessibleName = "txt5";
+            this.txt5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt5.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt5.Location = new System.Drawing.Point(183, 123);
+            this.txt5.Margin = new System.Windows.Forms.Padding(5);
+            this.txt5.MaxLength = 1;
+            this.txt5.Multiline = true;
+            this.txt5.Name = "txt5";
+            this.txt5.Size = new System.Drawing.Size(126, 82);
+            this.txt5.TabIndex = 4;
+            this.txt5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt5.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt1_KeyPress);
+            // 
+            // txt6
+            // 
+            this.txt6.AccessibleName = "txt6";
+            this.txt6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt6.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt6.Location = new System.Drawing.Point(338, 123);
+            this.txt6.Margin = new System.Windows.Forms.Padding(5);
+            this.txt6.MaxLength = 1;
+            this.txt6.Multiline = true;
+            this.txt6.Name = "txt6";
+            this.txt6.Size = new System.Drawing.Size(126, 82);
+            this.txt6.TabIndex = 5;
+            this.txt6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt6.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt1_KeyPress);
+            // 
+            // txt7
+            // 
+            this.txt7.AccessibleName = "txt7";
+            this.txt7.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt7.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt7.Location = new System.Drawing.Point(29, 226);
+            this.txt7.Margin = new System.Windows.Forms.Padding(5);
+            this.txt7.MaxLength = 1;
+            this.txt7.Multiline = true;
+            this.txt7.Name = "txt7";
+            this.txt7.Size = new System.Drawing.Size(126, 82);
+            this.txt7.TabIndex = 6;
+            this.txt7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt7.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt1_KeyPress);
+            // 
+            // txt8
+            // 
+            this.txt8.AccessibleName = "txt8";
+            this.txt8.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt8.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt8.Location = new System.Drawing.Point(183, 226);
+            this.txt8.Margin = new System.Windows.Forms.Padding(5);
+            this.txt8.MaxLength = 1;
+            this.txt8.Multiline = true;
+            this.txt8.Name = "txt8";
+            this.txt8.Size = new System.Drawing.Size(126, 82);
+            this.txt8.TabIndex = 7;
+            this.txt8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt8.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt1_KeyPress);
+            // 
+            // txt9
+            // 
+            this.txt9.AccessibleName = "txt9";
+            this.txt9.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt9.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt9.Location = new System.Drawing.Point(338, 226);
+            this.txt9.Margin = new System.Windows.Forms.Padding(5);
+            this.txt9.MaxLength = 1;
+            this.txt9.Multiline = true;
+            this.txt9.Name = "txt9";
+            this.txt9.Size = new System.Drawing.Size(126, 82);
+            this.txt9.TabIndex = 8;
+            this.txt9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt9.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt1_KeyPress);
+            // 
+            // btnSolve
+            // 
+            this.btnSolve.AccessibleName = "btnSolve";
+            this.btnSolve.BackColor = System.Drawing.Color.Blue;
+            this.btnSolve.FlatAppearance.BorderSize = 0;
+            this.btnSolve.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSolve.Font = new System.Drawing.Font("Anjoman Regular", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnSolve.ForeColor = System.Drawing.Color.White;
+            this.btnSolve.Location = new System.Drawing.Point(29, 346);
+            this.btnSolve.Name = "btnSolve";
+            this.btnSolve.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnSolve.Size = new System.Drawing.Size(126, 48);
+            this.btnSolve.TabIndex = 9;
+            this.btnSolve.Text = "حل خودکار";
+            this.btnSolve.UseVisualStyleBackColor = false;
+            this.btnSolve.Click += new System.EventHandler(this.btnSolve_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.AccessibleName = "btnReset";
+            this.btnReset.BackColor = System.Drawing.Color.Blue;
+            this.btnReset.FlatAppearance.BorderSize = 0;
+            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReset.Font = new System.Drawing.Font("Anjoman Regular", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnReset.ForeColor = System.Drawing.Color.White;
+            this.btnReset.Location = new System.Drawing.Point(183, 346);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnReset.Size = new System.Drawing.Size(126, 48);
+            this.btnReset.TabIndex = 10;
+            this.btnReset.Text = "پاک کردن";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.AccessibleName = "btnCheck";
+            this.btnCheck.BackColor = System.Drawing.Color.Blue;
+            this.btnCheck.FlatAppearance.BorderSize = 0;
+            this.btnCheck.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCheck.Font = new System.Drawing.Font("Anjoman Regular", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnCheck.ForeColor = System.Drawing.Color.White;
+            this.btnCheck.Location = new System.Drawing.Point(338, 346);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnCheck.Size = new System.Drawing.Size(126, 48);
+            this.btnCheck.TabIndex = 11;
+            this.btnCheck.Text = "بررسی نتیجه";
+            this.btnCheck.UseVisualStyleBackColor = false;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AccessibleName = "lblStatus";
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Anjoman Regular", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lblStatus.Location = new System.Drawing.Point(29, 428);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblStatus.Size = new System.Drawing.Size(297, 35);
+            this.lblStatus.TabIndex = 13;
+            this.lblStatus.Text = "متن نتیجه اینجا قرار خواهد گرفت.";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // Form1
             // 
+            this.AccessibleName = "MagicSquareGame";
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(493, 496);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnSolve);
+            this.Controls.Add(this.txt9);
+            this.Controls.Add(this.txt8);
+            this.Controls.Add(this.txt7);
+            this.Controls.Add(this.txt6);
+            this.Controls.Add(this.txt5);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "MagicSquareGame";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.TextBox txt5;
+        private System.Windows.Forms.TextBox txt6;
+        private System.Windows.Forms.TextBox txt7;
+        private System.Windows.Forms.TextBox txt8;
+        private System.Windows.Forms.TextBox txt9;
+        private System.Windows.Forms.Button btnSolve;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblStatus;
     }
 }
 
--- magic-square-game/Form1.cs
+++ magic-square-game/Form1.cs
@@ -16,5 +16,90 @@ namespace magic_square_game
         {
             InitializeComponent();
         }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // ۱. تعریف آرایه و گرفتن مقادیر
+                int[,] grid = new int[3, 3];
+                grid[0, 0] = int.Parse(txt1.Text); grid[0, 1] = int.Parse(txt2.Text); grid[0, 2] = int.Parse(txt3.Text);
+                grid[1, 0] = int.Parse(txt4.Text); grid[1, 1] = int.Parse(txt5.Text); grid[1, 2] = int.Parse(txt6.Text);
+                grid[2, 0] = int.Parse(txt7.Text); grid[2, 1] = int.Parse(txt8.Text); grid[2, 2] = int.Parse(txt9.Text);
+
+                // ۲. بررسی تکراری نبودن (استفاده از HashSet)
+                HashSet<int> numbers = new HashSet<int>();
+                for (int r = 0; r < 3; r++)
+                    for (int c = 0; c < 3; c++)
+                        if (grid[r, c] < 1 || grid[r, c] > 9 || !numbers.Add(grid[r, c]))
+                        {
+                            lblStatus.Text = "اعداد باید بین ۱ تا ۹ و غیرتکراری باشند!";
+                            return;
+                        }
+
+                // ۳. بررسی جمع‌ها
+                bool isMagic = true;
+                // جمع سطرها، ستون‌ها و قطرها رو چک می‌کنیم
+                if ((grid[0, 0] + grid[0, 1] + grid[0, 2] != 15) || // سطر ۱
+                    (grid[1, 0] + grid[1, 1] + grid[1, 2] != 15) || // سطر ۲
+                    (grid[2, 0] + grid[2, 1] + grid[2, 2] != 15) || // سطر ۳
+                    (grid[0, 0] + grid[1, 0] + grid[2, 0] != 15) || // ستون ۱
+                    (grid[0, 1] + grid[1, 1] + grid[2, 1] != 15) || // ستون ۲
+                    (grid[0, 2] + grid[1, 2] + grid[2, 2] != 15) || // ستون ۳
+                    (grid[0, 0] + grid[1, 1] + grid[2, 2] != 15) || // قطر اصلی
+                    (grid[0, 2] + grid[1, 1] + grid[2, 0] != 15))   // قطر فرعی
+                {
+                    isMagic = false;
+                }
+
+                if (isMagic)
+                {
+                    lblStatus.ForeColor = Color.Green;
+                    lblStatus.Text = "تبریک! این یک مربع جادویی است.";
+                    MessageBox.Show("شما برنده شدید!");
+                }
+                else
+                {
+                    lblStatus.ForeColor = Color.Red;
+                    lblStatus.Text = "جمع سطرها یا ستون‌ها ۱۵ نیست. دوباره تلاش کنید.";
+                }
+            }
+            catch
+            {
+                MessageBox.Show("لطفاً تمام خانه‌ها را با عدد پر کنید.");
+            }
+        }
+
+        private void txt1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnSolve_Click(object sender, EventArgs e)
+        {
+            // پاسخ استاندارد مربع جادویی ۳در۳ طبق الگوریتم سیامی
+            txt1.Text = "8"; txt2.Text = "1"; txt3.Text = "6";
+            txt4.Text = "3"; txt5.Text = "5"; txt6.Text = "7";
+            txt7.Text = "4"; txt8.Text = "9"; txt9.Text = "2";
+
+            lblStatus.ForeColor = Color.Blue;
+            lblStatus.Text = "مربع توسط هوش کامپیوتر حل شد.";
+            this.BackColor = SystemColors.Control;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            // پاک کردن متن تمام تکست‌باکس‌ها
+            txt1.Clear(); txt2.Clear(); txt3.Clear();
+            txt4.Clear(); txt5.Clear(); txt6.Clear();
+            txt7.Clear(); txt8.Clear(); txt9.Clear();
+
+            // برگرداندن وضعیت فرم به حالت اولیه
+            lblStatus.Text = "";
+            this.BackColor = SystemColors.Control;
+        }
     }
 }
